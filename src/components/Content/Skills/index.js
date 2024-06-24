58206d6d07ba12d5f040b5bd3b4a323b12831817
Achyut Misra <achyutmishra311@gmail.com>
2024-06-25 01:24:11 +0530

    Data added

--- src/components/Content/Skills/index.js
+++ src/components/Content/Skills/index.js
@@ -1,19 +1,34 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
 import OutlinedCard from "../../ReusuableComponents/Card";
-import Stack from '@mui/material/Stack';
+import Stack from "@mui/material/Stack";
 
-const Skills = ({reference}) => {
+const Skills = ({ reference }) => {
+  let SkillJSX = (
+    <>
+      <ul>
+        <li></li>
+      </ul>
+    </>
+  );
+  
   return (
     <>
-      <Typography ref={reference} variant="h6" gutterBottom sx={{ textAlign: "center" }}>
+      <Typography
+        ref={reference}
+        variant="h6"
+        gutterBottom
+        sx={{ textAlign: "center" }}
+      >
         Skills
       </Typography>
-      <Stack style={{justifyContent:"center", alignItems:"center"}}>
-        <OutlinedCard style={{ width: "95%", padding:1 }} />
-        <OutlinedCard style={{ width: "95%" ,  padding:1}} />
-        <OutlinedCard style={{ width: "95%" ,  padding:1}} />
-        <OutlinedCard style={{ width: "95%" ,  padding:1}} />
+      <Stack style={{ justifyContent: "center", alignItems: "center" }}>
+        <OutlinedCard
+          style={{ width: "95%", padding: 1 }}
+          heading={"IT Awards"}
+          secondaryHeading={"2018 - Present"}
+          body={AwardsJSX}
+        />
       </Stack>
     </>
   );
